56fb1961f939087f2f723881e51d7d541b1e2c73
kashan <kashan@fudata.cn>
2024-01-31 15:00:54 +0800

    add uint64

--- tests/test_kkot.cpp
+++ tests/test_kkot.cpp
@@ -7,7 +7,20 @@
 using namespace std;
 using namespace oc;
 using namespace fucrypto;
-
+static uint64_t get64() {
+  int tmp1 = rand();
+  int tmp2 = rand();
+  int tmp3 = rand();
+  uint64_t tmp;
+  char* p = (char*)&tmp;
+  memcpy(p, &tmp1, 3);
+  memcpy(p + 3, &tmp2, 3);
+  memcpy(p + 6, &tmp3, 2);
+  return tmp;
+}
+static uint64_t get_mask(uint64_t bit_l) {
+  return bit_l >= 64 ? -1 : (uint64_t(1) << bit_l) - uint64_t(1);
+}
 void test_kkot_sender(int num_ot, vector<vector<block>>& out_masks,
                       config_param& param) {
   int numOTExt = num_ot;
@@ -185,6 +198,7 @@ void test_kkot_receiver2(const vector<int>& r_i, vector<uint8_t>& out_data,
   SPDLOG_LOGGER_INFO(spdlog::default_logger(), "kkot recver recvBytes:{} B",
                      (&c)->recv_bytes_count());
 }
+
 int main_test2(int argc, char** argv) {
   srand(time(NULL));
   bool is_power = false;
@@ -256,17 +270,135 @@ int main_test2(int argc, char** argv) {
   return 0;
 }
 
-uint64_t get64() {
-  int tmp1 = rand();
-  int tmp2 = rand();
-  int tmp3 = rand();
-  uint64_t tmp;
-  char* p = (char*)&tmp;
-  memcpy(p, &tmp1, 3);
-  memcpy(p + 3, &tmp2, 3);
-  memcpy(p + 6, &tmp3, 2);
-  return tmp;
+void test_kkot_sender_uint64(int num_ot, vector<vector<uint64_t>>& in_data,
+                             config_param& param, int N, int bit_l) {
+  int numOTExt = num_ot;
+  connection c(0, "127.0.0.1", 9001);
+  kkot_sender kkot(param);
+  SPDLOG_LOGGER_INFO(spdlog::default_logger(), "kkot_sender create");
+#if 0
+  int base_ot_num = kkot.get_base_ot_count();
+  BitVector base_choices(base_ot_num);
+  PRNG rng(sysRandomSeed());
+  base_choices.randomize(rng);
+  //   iknp_receiver iknp;
+  //   ote_receiver* ote = &iknp;
+  auto ote = new_ote_receiver(param);
+  vector<block> single_keys(base_ot_num);
+  int fg = ote->receive(base_choices, single_keys, &c);
+  if (fg) {
+    SPDLOG_LOGGER_ERROR(spdlog::default_logger(), "ote error");
+  }
+
+  kkot.set_base_ot(base_choices, single_keys);
+#endif
+  kkot.send(&c, in_data, N, bit_l);
+  SPDLOG_LOGGER_INFO(spdlog::default_logger(), "kkot sender sendBytes:{} B",
+                     (&c)->send_bytes_count());
+  SPDLOG_LOGGER_INFO(spdlog::default_logger(), "kkot sender recvBytes:{} B",
+                     (&c)->recv_bytes_count());
 }
+
+void test_kkot_receiver_uint64(const vector<int>& r_i,
+                               vector<uint64_t>& out_data, config_param& param,
+                               int N, int bit_l) {
+  //   int numOTExt = choices.size();
+  connection c(1, "127.0.0.1", 9001);
+  kkot_receiver kkot(param);
+  SPDLOG_LOGGER_INFO(spdlog::default_logger(), "kkrt_receiver create");
+
+#if 0
+  int base_ot_num = kkot.get_base_ot_count();
+  //   iknp_sender iknp;
+  //   ote_sender* ote = &iknp;
+  auto ote = new_ote_sender(param);
+  vector<array<block, 2>> pair_keys(base_ot_num);
+  int fg = ote->send(pair_keys, &c);
+  if (fg) {
+    SPDLOG_LOGGER_ERROR(spdlog::default_logger(), "ote sender error");
+  }
+  //
+  kkot.set_base_ot(pair_keys);
+#endif
+  kkot.recv(&c, r_i, out_data, N, bit_l);
+  //  解密
+  SPDLOG_LOGGER_INFO(spdlog::default_logger(), "kkot recver sendBytes:{} B",
+                     (&c)->send_bytes_count());
+  SPDLOG_LOGGER_INFO(spdlog::default_logger(), "kkot recver recvBytes:{} B",
+                     (&c)->recv_bytes_count());
+}
+
+int main_test_uint64(int argc, char** argv) {
+  srand(time(NULL));
+  bool is_power = false;
+  int num_Ote = 1 << 19;
+  if (argc > 1) {
+    if (is_power)
+      num_Ote = 1 << atoi(argv[1]);
+    else
+      num_Ote = atoi(argv[1]);
+  }
+  //   if (argc > 1) num_Ote = atoi(argv[1]);
+  int N = 16, bit_l = 16;
+  if (argc > 2) N = atoi(argv[2]);
+  if (argc > 3) bit_l = atoi(argv[3]);
+  config_param param;
+  param.hasher_name = "blake3";
+  uint64_t mask = get_mask(bit_l);
+  printf("===== mask:%llx\n", mask);
+  vector<vector<uint64_t>> inputs(num_Ote);
+  for (size_t i = 0; i < num_Ote; i++) {
+    for (size_t j = 0; j < N; j++) {
+      inputs[i].push_back((get64() & mask));
+    }
+  }
+  vector<int> choices(num_Ote);
+  for (size_t i = 0; i < num_Ote; i++) {
+    choices[i] = rand() % N;
+  }
+  time_point tp;
+  //
+  cout << "========== start time " << tp.get_time_piont_ms() << " ms" << endl;
+  //   vector<vector<block>> out_masks;
+  thread th1(test_kkot_sender_uint64, num_Ote, ref(inputs), ref(param), N,
+             bit_l);
+
+  vector<uint64_t> out_data;
+  thread th2(test_kkot_receiver_uint64, ref(choices), ref(out_data), ref(param),
+             N, bit_l);
+  th1.join();
+  th2.join();
+  cout << "========== end time " << tp.get_time_piont_ms() << " ms" << endl;
+  //   check
+  for (size_t i = 0; i < num_Ote; i++) {
+    if (i < 3 && i < num_Ote) {
+      // if (i < num_Ote) {
+      //   set<block> tmp;
+      for (size_t j = 0; j < N; j++) {
+        cout << "[" << j << "]" << uint64_t(inputs[i][j]) << endl;
+        // tmp.insert(out_masks[i][j]);
+      }
+      //   if (tmp.size() != N) {
+      //     cout << "tmp.size()!=N error i:" << i << endl;
+      //     return 0;
+      //   }
+      cout << "[" << (uint64_t)choices[i] << "]" << uint64_t(out_data[i])
+           << endl;
+      cout << "[" << (uint64_t)choices[i] << "]"
+           << uint64_t(inputs[i][choices[i]]) << endl;
+      cout << endl;
+    }
+    if (out_data[i] != inputs[i][(uint64_t)choices[i]]) {
+      cout << "kkot check not ok ,i:" << i << endl;
+      return 0;
+    }
+    // return 0;
+  }
+  cout << "kkot2 check ok" << endl;
+  cout << "ote_num:" << num_Ote << ",N:" << N << ",bit_l:" << bit_l << endl;
+  return 0;
+}
+
 void test_gen_wh_code() {
   srand(time(NULL));
   //   uint64_t WH_Code[256][8];
@@ -354,10 +486,12 @@ void test_N_bit_l() {
   }
   printf("ret:%d,ok:%d\n", (ret ^ k_r), inputs[r]);
 }
+
 int main(int argc, char** argv) {
   SPDLOG_LOGGER_INFO(spdlog::default_logger(), "=== test kkot ===");
   //   main_test(argc, argv);
-  main_test2(argc, argv);
+  //   main_test2(argc, argv);
+  main_test_uint64(argc, argv);
   //   int tmp[4];
   //   test_N_bit_l();
   for (int i = 0; i < 4; i++) {
@@ -366,6 +500,13 @@ int main(int argc, char** argv) {
   cout << endl;
   uint8_t mask = (1 << 8) - 1;
   printf(">>>>>>>>>.mask:%d\n", mask);
+  printf("get_mask l==0  :%llx\n", get_mask(0));
+  printf("get_mask l==1  :%llx\n", get_mask(1));
+  printf("get_mask l==8  :%llx\n", get_mask(8));
+  printf("get_mask l==16 :%llx\n", get_mask(16));
+  printf("get_mask l==32 :%llx\n", get_mask(32));
+  printf("get_mask l==63 :%llx\n", get_mask(63));
+  printf("get_mask l==64 :%llx\n", get_mask(64));
   //   test_gen_wh_code();
   return 0;
 }
